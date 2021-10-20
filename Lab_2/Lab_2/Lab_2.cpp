2eeae7d52b5ce23f94bae68c7f94f91fdbc734f8
strannik1105 <58438982+strannik1105@users.noreply.github.com>
2021-10-20 23:09:48 +0700

    Add files via upload

---
+++ Lab_2/Lab_2/Lab_2.cpp
@@ -0,0 +1,17 @@
+﻿#include "Triangle.h"
+#include <iostream>
+#include <Windows.h>
+
+//using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+	while (true)
+	{
+		system("clear");
+		Triangle* triangle = new Triangle();
+		triangle->PrintInfo();
+	}
+	system("pause");
+}
