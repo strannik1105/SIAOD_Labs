dfd5043eddd26782b80769e3c20646fbe15c7cc1
strannik1105 <58438982+strannik1105@users.noreply.github.com>
2021-10-24 23:38:51 +0700

    Add files via upload

--- Lab1_Task1/Lab1_Task1/Lab1_Task1.cpp
+++ Lab1_Task1/Lab1_Task1/Lab1_Task1.cpp
@@ -10,8 +10,11 @@
 
 using namespace std;
 
+
+//функция проверяющая, простое ли число ch
 bool Is_Simple(unsigned long long ch)
 {
+    //проерка всех возможных множителей от i до корня из проверяемого числа
     for (unsigned long long i = 2; i <= sqrt(ch); i++)
     {
         if (ch % i == 0)
@@ -22,16 +25,22 @@ bool Is_Simple(unsigned long long ch)
 
 int main()
 {
+    //подключение файлов
     ifstream fin("INPUT.TXT");
     ofstream fout("OUTPUT.TXT");
-    unsigned long long m = 0;
+    //границы поиска и ввод
+    unsigned long long m = 0;   
     unsigned long long n = 0;
     fin >> m;
     fin >> n;
+    //флаг отвечаюший за нахождение хотябы одного совершенного числа
     bool exist = false;
+    //поиск среди всех простых чисел Мерсера согласно формуле совершенного числа
+    //пропуск всех чисел меньших m
     unsigned long long i = 2;
     while (pow(2, i - 1) * (pow(2, i) - 1) < m)
         i++;
+    //поиск до n
     while ((pow(2, i - 1) * (pow(2, i) - 1)) <= n)
     {
         if (Is_Simple(pow(2, i) - 1))
@@ -41,6 +50,7 @@ int main()
         }
         i++;
     }
+    //вывод, в случае, если число не было найдено
     if (!exist)
         fout << "Absent" << endl;
     return 0;
